bbf56d52416a9a390d07943c1f1520a21fc2bcfe
Alex Larrabee <nekotree556@gmail.com>
2020-05-18 14:30:02 -0700

    work in progress

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -30,7 +30,7 @@ export default class App extends Component {
           <Switch>
             <PrivateRoute
               exact
-              path={'/'}
+              path={'/dashboard'}
               component={DashboardRoute}
             />
             <PrivateRoute
--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,10 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import config from '../../config';
 
 class DashboardRoute extends Component {
+
+
+  state = {
+
+    language: []
+
+  }
+
+  componentDidMount = () => {
+
+    fetch(`${config.API_ENDPOINT}/language`)
+      .then((data) => { console.log(data) })
+      .then((result) => {
+        this.setState({
+          language: result
+        })
+      })
+
+  }
+
+
+
   render() {
+    console.log(this.state.language)
     return (
       <section>
-        implement and style me
+        <p>{this.state.language.name}</p>
       </section>
     );
   }
