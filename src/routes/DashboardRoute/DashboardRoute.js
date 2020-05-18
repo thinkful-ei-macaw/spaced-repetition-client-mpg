0676f38d4e423ce0a7900aa899d262e98d2aca88
Muhiddin Kurbonov <muhiddin.m.kurbonov@gmail.com>
2020-05-18 16:33:52 -0700

    some changes

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
+import config from '../../config';
+import TokenService from '../../services/token-service';
 
 class DashboardRoute extends Component {
+  state = {
+    language: {},
+    words: []
+  }
+
+  componentDidMount() {
+    return fetch(`${config.API_ENDPOINT}/language`, {
+      headers: {
+        authorization: `bearer ${TokenService.getAuthToken()}`
+      }
+    })
+    .then((res) => (!res.ok ? res.json()
+    .then((e) => Promise.reject(e)) : res.json()))
+    .then(obj => this.setState({language: obj.language, words: obj.words}));  
+  }
+  
+
   render() {
+   // console.log(this.state.language);
     return (
       <section>
-        implement and style me
+        <h2>{this.state.language.name}</h2>
+        <button>Start practicing!</button>
+        <h3>Words to practice</h3>
+        <ul>{this.state.words.map(word => {
+          return <li key={word.id}>{word.original.toUpperCase()}  |
+          correct answer count: {word.correct_count} |
+          incorrect answer count: {word.incorrect_count}</li>
+        })}</ul>
+        <h4>Total score: {this.state.language.total_score}</h4>
       </section>
     );
   }
