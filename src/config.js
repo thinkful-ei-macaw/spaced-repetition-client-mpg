9f2c9805c331e2a492c1db083b23aa69da5d2222
Muhiddin Kurbonov <muhiddin.m.kurbonov@gmail.com>
2020-05-18 17:18:33 -0700

    sc

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'http://localhost:8000/api' || 'https://duolang-server.herokuapp.com/api',
   TOKEN_KEY: 'change-this-secret',
 }
