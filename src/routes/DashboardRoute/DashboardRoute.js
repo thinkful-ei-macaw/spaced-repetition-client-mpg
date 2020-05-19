970e3b4ad2b2a346f6b557235deb8df786b82885
Alex Larrabee <nekotree556@gmail.com>
2020-05-18 17:01:34 -0700

    work in progress

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -6,21 +6,24 @@ class DashboardRoute extends Component {
 
   state = {
 
-    language: []
+    language: {},
+    words: []
 
   }
 
   componentDidMount = () => {
 
-    fetch(`${config.API_ENDPOINT}/language`, {
+    return fetch(`${config.API_ENDPOINT}/language`, {
       headers: {
         authorization: `bearer ${TokenService.getAuthToken()}`
       }
     })
-      .then((data) => { console.log(data) })
-      .then((result) => {
+      .then((res) => (!res.ok ? res.json()
+        .then((e) => Promise.reject(e)) : res.json()))
+      .then(obj => {
         this.setState({
-          language: result
+          language: obj.language,
+          words: obj.words
         })
       })
 
@@ -28,14 +31,36 @@ class DashboardRoute extends Component {
 
 
 
+
+
+
+
+
   render() {
     console.log(this.state.language)
+    console.log(this.state.words)
     return (
-      <section>
-        <p>{this.state.language.name}</p>
-      </section>
+      <div>
+        <section>
+          <h2>{this.state.language.name}</h2>
+          <button>Start Practicing</button>
+          <ul>
+            {this.state.words.map(word => {
+              return <li key={word.id}>
+                {word.original.toUpperCase()}  =  {word.translation} | Correct Count :  {word.correct_count} | Incorrect Count :  {word.incorrect_count}
+              </li>
+
+            })}
+          </ul>
+          <h3>Total Score : {this.state.language.total_score}</h3>
+        </section>
+      </div>
     );
   }
+
 }
+
+
+
 
 export default DashboardRoute
